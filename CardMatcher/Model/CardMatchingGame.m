07cb9b187021e80c3e90434bd4eecbbd04af89f1
Horace Williams <horace.williams@livingsocial.com>
2014-03-23 18:25:16 -0600

    implement chooseCardAtIndex for match game

--- CardMatcher/Model/CardMatchingGame.m
+++ CardMatcher/Model/CardMatchingGame.m
@@ -15,6 +15,10 @@
 
 @implementation CardMatchingGame
 
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+
+
 - (instancetype)initWithCardCount:(NSUInteger)count
                         usingDeck:(Deck *)deck
 {
@@ -46,7 +50,32 @@
 
 - (void)chooseCardAtIndex:(NSUInteger)index
 {
-    
+    Card *chosenCard = [self cardAtIndex:index];
+
+    if (!chosenCard.isMatched) {
+        if (chosenCard.isChosen) {
+            chosenCard.chosen = NO;
+        } else {
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    NSLog(@"MATCHING CARDS");
+                    NSLog(@"Got a match between %@ and %@", chosenCard.contents, otherCard.contents);
+                    int matchScore = [chosenCard match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched = YES;
+                        chosenCard.matched = YES;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break;
+                }
+            }
+            chosenCard.chosen = YES;
+        }
+    }
+
 }
 
 @end
--- CardMatcherTests/CardMatchingGameTest.m
+++ CardMatcherTests/CardMatchingGameTest.m
@@ -25,6 +25,7 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
     self.testCard = [[Card alloc] init];
     self.testCard.contents = @"testCard";
+    self.testCard.matched = NO;
     self.testDeck = [[Deck alloc] init];
     [self.testDeck addCard:self.testCard];
 }
@@ -33,6 +34,7 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    
 }
 
 - (void)testWillNotInitForEmptyDeck
@@ -56,6 +58,120 @@
     CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:1
                                                                    usingDeck:self.testDeck];
     XCTAssertNil([testGame cardAtIndex:4]);
+}
+
+- (void)testChooseCardAtIndexUnchoosesAChosenCard {
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:1
+                                                                   usingDeck:self.testDeck];
+    [testGame cardAtIndex:0].chosen = YES;
+    [testGame chooseCardAtIndex:0];
+    XCTAssertEqual([testGame cardAtIndex:0].isChosen, NO);
+}
+
+- (void)testChooseCardAtIndexChoosesTheCard {
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:1
+                                                                   usingDeck:self.testDeck];
+    [testGame cardAtIndex:0].chosen = NO;
+    [testGame chooseCardAtIndex:0];
+    XCTAssertEqual([testGame cardAtIndex:0].isChosen, YES);
+}
+
+- (void)testChooseCardAtIndexPassesOvermatchedCard {
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:1
+                                                                   usingDeck:self.testDeck];
+    [testGame cardAtIndex:0].chosen = YES;
+    [testGame cardAtIndex:0].matched = YES;
+    [testGame chooseCardAtIndex:0];
+    XCTAssertEqual([testGame cardAtIndex:0].isChosen, YES);
+}
+
+- (void)testChooseCardAtIndexIgnoresMatchingUnchosenCards {
+    Card *secondCard = [[Card alloc] init];
+    secondCard.contents = self.testCard.contents;
+    secondCard.chosen = NO;
+    [self.testDeck addCard:secondCard];
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:2
+                                                                   usingDeck:self.testDeck];
+    
+    [testGame chooseCardAtIndex:0];
+    XCTAssertEqual(testGame.score, 0);
+}
+
+- (void)testChooseCardAtIndexGivesScoreBonusForMatchingChosenCards {
+    Card *firstCard = [[Card alloc] init];
+    Card *secondCard = [[Card alloc] init];
+    firstCard.contents = @"a card";
+    secondCard.contents = @"a card";
+    
+    Deck *deck = [[Deck alloc] init];
+    [deck addCard:firstCard];
+    [deck addCard:secondCard];
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:2
+                                                                   usingDeck:deck];
+    [testGame chooseCardAtIndex:0]; //choose one card so our subsequent choice can match it
+    [testGame chooseCardAtIndex:1];
+
+    XCTAssertTrue(testGame.score > 0);
+}
+
+- (void)testChooseCardAtIndexUpdatesMatchStatusForChosenMatchingCards {
+    Card *firstCard = [[Card alloc] init];
+    Card *secondCard = [[Card alloc] init];
+    firstCard.contents = @"same contents";
+    secondCard.contents = @"same contents";
+
+    Deck *deck = [[Deck alloc] init];
+    [deck addCard:firstCard];
+    [deck addCard:secondCard];
+    
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:2
+                                                                   usingDeck:deck];
+    
+    [testGame chooseCardAtIndex:0]; //choose one card so our subsequent choice can match it
+    [testGame chooseCardAtIndex:1];
+    
+    XCTAssertTrue(testGame.score > 0);
+
+    XCTAssertEqual(firstCard.isMatched, YES);
+    XCTAssertEqual(secondCard.isMatched, YES);
+}
+
+- (void)testChooseCardAtDecreasesScoreForMisMatch {
+    Card *firstCard = [[Card alloc] init];
+    Card *secondCard = [[Card alloc] init];
+    firstCard.contents = @"a card";
+    secondCard.contents = @"a different card";
+    
+    Deck *deck = [[Deck alloc] init];
+    [deck addCard:firstCard];
+    [deck addCard:secondCard];
+    
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:2
+                                                                   usingDeck:deck];
+    
+    [testGame chooseCardAtIndex:0]; //choose one card so our subsequent choice can match it
+    [testGame chooseCardAtIndex:1];
+    XCTAssertTrue(testGame.score < 0);
+}
+
+- (void)testChooseCardAtUnChoosesMisMatchedCard {
+    Card *firstCard = [[Card alloc] init];
+    Card *secondCard = [[Card alloc] init];
+    firstCard.contents = @"a card";
+    secondCard.contents = @"a different card";
+    
+    Deck *deck = [[Deck alloc] init];
+    [deck addCard:firstCard];
+    [deck addCard:secondCard];
+    
+    CardMatchingGame *testGame = [[CardMatchingGame alloc] initWithCardCount:2
+                                                                   usingDeck:deck];
+    
+    [testGame chooseCardAtIndex:0]; //choose one card so our subsequent choice can match it
+    [testGame chooseCardAtIndex:1];
+    // first card gets flips back, second one stays up
+    XCTAssertFalse([testGame cardAtIndex:0].isChosen);
+    XCTAssertTrue([testGame cardAtIndex:1].isChosen);
 }
 
 @end
