6f2685e2d488cdd99a7cdbdced42dff3af5dd074
Horace Williams <horace.d.williams@gmail.com>
2014-04-01 21:59:00 -0400

    wiring up mode change buttons so user can change between 2-card and 3-card mode when a game is started

--- CardMatcher/CardMatcherViewController.m
+++ CardMatcher/CardMatcherViewController.m
@@ -15,6 +15,8 @@
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *currentGame;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *changeModeButton;
+@property (strong, nonatomic) NSString *gameMode;
 @end
 
 @implementation CardMatcherViewController
@@ -34,23 +36,41 @@
     _currentGame = self.createGame;
 }
 
+- (NSString *)gameMode {
+    if(!_gameMode) _gameMode = [CardMatchingGame matchMode2Card];
+    return _gameMode;
+}
+
 - (CardMatchingGame *) createGame {
-    return [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count
-                                             usingDeck:self.createDeck];}
+    return [[CardMatchingGame alloc] initWithMatchMode:[self gameMode]
+                                             usingDeck:[self createDeck]
+                                         withCardCount:[self.cardButtons count]];
+}
 
 - (IBAction)touchCardButton:(UIButton *)sender {
+    
+    //disable mode switching
     int chosenButtonIndex = [self.cardButtons indexOfObject:sender];
     [self.currentGame chooseCardAtIndex:chosenButtonIndex];
+    [self disableChangeModeButton];
     [self updateUi];
 }
 
 - (IBAction)touchNewGameButton:(UIButton *)sender {
+    [self enableChangeModeButton];
     [self resetCurrentGame];
     [self updateUi];
 }
 
 - (IBAction)changeGameMode:(UISegmentedControl *)sender {
-    NSLog(@"updated game mode to %@", [sender titleForSegmentAtIndex:sender.selectedSegmentIndex]);
+    NSString *selectedMode = [sender titleForSegmentAtIndex:sender.selectedSegmentIndex];
+    if ([selectedMode isEqual: @"3-card matching"]) {
+        self.currentGame.matchMode = [CardMatchingGame matchMode3Card];
+    } else if ([selectedMode isEqual: @"2-card matching"]) {
+        self.currentGame.matchMode = [CardMatchingGame matchMode2Card];
+    } else {
+        //do nothing
+    }
 }
 
 - (void) updateUi {
@@ -61,6 +81,18 @@
         [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
         cardButton.enabled = !card.isMatched;
         self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.currentGame.score];
+    }
+}
+
+- (void) disableChangeModeButton {
+    for (int i = 0; i < [self.changeModeButton numberOfSegments]; i++) {
+        [self.changeModeButton setEnabled:NO forSegmentAtIndex:i];
+    }
+}
+
+- (void) enableChangeModeButton {
+    for (int i = 0; i < [self.changeModeButton numberOfSegments]; i++) {
+        [self.changeModeButton setEnabled:YES forSegmentAtIndex:i];
     }
 }
 
